1707332de01da049bc4f0b3114e86e646189bc35
mingmanyuan <>
2020-12-01 15:08:18 -0800

    more generalization and load State Location data

--- Final_Project.py
+++ Final_Project.py
@@ -34,6 +34,35 @@ def getH1B(file_name):
 getH1B('H-1B FY2018.xlsx')
 getH1B('H1B FY2016.xlsx')
 
+# The file 'H-1B FY2018.xlsx' is around 200 mb, which would take around 10 mins (~8 mins for 'H1B FY2016.xlsx')
+# on my MacBook Air2020 to read, therefore I would not recommend reading this file directly. For grading purpose,
+# I would suggest directly read the file 'H-1B FY2018 Sub.csv' which I have pre-stored in the repo. I also commented out
+# the code (see below) which I have built and ran to transform the original data set to a smaller one. This subset H1b table only contains
+# relevant columns that I would use later in the feature engineering and modeling process. And I apply this on H1B Y2016
+# data as well.
+
+# H1b2018 = pd.read_excel('H-1B FY2018.xlsx')
+# H1b2016 = pd.read_excel('H1B FY2016.xlsx')
+
+
+# def subH1B(dt):
+#     dt = dt.loc[dt['CASE_STATUS'] == 'CERTIFIED'].filter(['CASE_NUMBER', 'EMPLOYER_NAME',
+#                                                           'EMPLOYER_STATE', 'JOB_TITLE', 'SOC_CODE',
+#                                                           'PREVAILING_WAGE', 'PW_UNIT_OF_PAY',
+#                                                           'WORKSITE_STATE'], axis=1)
+#     return dt
+
+
+# H1b2018_sub = subH1B(H1b2018)
+# H1b2018_sub.to_csv('H-1B FY2018 Sub.csv', index=False)
+
+# H1b2016_sub = subH1B(H1b2016)
+# H1b2016_sub.to_csv('H-1B FY2016 Sub.csv', index=False)
+
+
+StateH1B18 = pd.read_csv('H-1B FY2018 Sub.csv')
+StateH1B16 = pd.read_csv('H-1B FY2016 Sub.csv')
+
 
 def getTechIndex(filename):
     TechIndex = []
@@ -65,6 +94,10 @@ def getTechIndex(filename):
 
 getTechIndex(['State Technology and Sciencxe Index Y2018.csv',
               'State Technology and Sciencxe Index Y2016.csv'])
+
+
+StateTech18 = pd.read_csv('State Technology and Sciencxe Index Y2018.csv')
+StateTech16 = pd.read_csv('State Technology and Sciencxe Index Y2016.csv')
 
 
 def downloadCensus(vars):
@@ -148,6 +181,55 @@ def derivedColumns(statedata):
     return statedata
 
 
-statedata = derivedColumns(statedata)
-statedata[0].to_csv('2017_Census_State_data.csv', index=False)
-statedata[1].to_csv('2015_Census_State_data.csv', index=False)
+def storeStateCensus(statedata, filename):
+    statedata = derivedColumns(statedata)
+    for m, n in zip(range(len(statedata)), range(len(filename))):
+        statedata[m].to_csv(filename[n], index=False)
+
+
+storeStateCensus(statedata, ['2017_Census_State_data.csv', '2015_Census_State_data.csv'])
+StateCensus17 = pd.read_csv('2017_Census_State_data.csv')
+StateCensus15 = pd.read_csv('2015_Census_State_data.csv')
+# statedata = derivedColumns(statedata)
+# statedata[0].to_csv('2017_Census_State_data.csv', index=False)
+# statedata[1].to_csv('2015_Census_State_data.csv', index=False)
+
+
+def getStatesLoc(filename):
+    # Call this function to download the latitude and longitude of each US state.
+    # Extract the table from the website 'ttps://developers.google.com/public-data/docs/canonical/states_csv'
+    # Call this function and save a csv file called 'filename' at the repo
+    StatesLoc = []
+    states_loc_url = 'https://developers.google.com/public-data/docs/canonical/states_csv'
+    html = urllib2.urlopen(states_loc_url).read()
+    soup = BeautifulSoup(html)
+    #  soup.find_all("table") will return a result set containing bs4.elements
+    #  Since there is only table on this website, I make the index equal to 0
+    StatesLongLat = soup.find_all("table")[0]
+    states_long_lat = []
+    # each state is inserted in 'tr' tags
+    for state_row in StatesLongLat.findAll('tr'):
+        # each cell of one particular row is inserted in 'td' tags, and created a new set columns to store values of all cell
+        values = state_row.findAll('td')
+        state_long_lat_set = []
+        for value in values:
+            # store each element in the set columns in the new list output_row
+            state_long_lat_set.append(value.text)
+        states_long_lat.append(state_long_lat_set)
+    StatesLoc.append(states_long_lat)
+    for i in range(len(StatesLoc)):
+        global path
+        with open(os.path.join(path, filename), 'w') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerows(StatesLoc[i])
+
+
+def loadStateLoc(filename):
+    # Call this function to read the State Locatoion.csv file into a clean dataframe
+    getStatesLoc(filename)
+    StateLoc = pd.read_csv(filename, header=None)
+    StateLoc.columns = ['stateabb', 'latitude', 'longitude', 'statename']
+    return StateLoc
+
+
+StateLoc = loadStateLoc('State Location.csv')
