0cead3e9d31982514efccad7a87773930e0e8b65
mingmanyuan <>
2020-11-30 22:57:11 -0800

    futher generalize the function

--- Final_Project.py
+++ Final_Project.py
@@ -32,46 +32,53 @@ def getH1B(file_name):
 
 
 getH1B('H-1B FY2018.xlsx')
+getH1B('H1B FY2016.xlsx')
 
 
-def getTechIndex2018(filename):
-    state_tech_and_science_org_url = 'http://statetechandscience.org/statetech.taf?page=overall-ranking&composite=tswf'
-    html = urllib2.urlopen(state_tech_and_science_org_url).read()
-    soup = BeautifulSoup(html)
+def getTechIndex(filename):
+    TechIndex = []
+    for i in range(len(filename)):
+        state_tech_and_science_org_url = 'http://statetechandscience.org/statetech.taf?page=overall-ranking&composite=tswf'
+        html = urllib2.urlopen(state_tech_and_science_org_url).read()
+        soup = BeautifulSoup(html)
     #  soup.find_all("table") will return a result set containing bs4.elements
-    # I use the index equal to 2 here as I want to extract data of Year 2018, which is inserted as the second element of bs4.element result set
-    TechIndex2018 = soup.find_all("table")[2]
-    state_index = []
-    # each row of table2018 is inserted in 'tr' tags
-    for state_row in TechIndex2018.findAll('tr'):
-        # each cell of one particular row is inserted in 'td' tags, and created a new set columns to store values of all cell
-        values = state_row.findAll('td')
-        state_index_set = []
-        for value in values:
-            # store each element in the set columns in the new list output_row
-            state_index_set.append(value.text)
-        state_index.append(state_index_set)
-    global path
-    with open(os.path.join(path, filename), 'w') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerows(state_index)
+    # When the index equal to 2, the function would extract data of Year 2018, which is inserted as the second element of bs4.element result set
+    # When the index equal to 3, the function would extract data of Year 2016, which is inserted as the third element of bs4.element result set
+        TechIndex1yr = soup.find_all("table")[i+2]
+        state_index = []
+    # each row of table2018/2016 is inserted in 'tr' tags
+        for state_row in TechIndex1yr.findAll('tr'):
+            # each cell of one particular row is inserted in 'td' tags, and created a new set columns to store values of all cell
+            values = state_row.findAll('td')
+            state_index_set = []
+            for value in values:
+                # store each element in the set columns in the new list output_row
+                state_index_set.append(value.text)
+            state_index.append(state_index_set)
+        TechIndex.append(state_index)
+    for m, n in zip(range(len(TechIndex)), range(len(filename))):
+        global path
+        with open(os.path.join(path, filename[n]), 'w') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerows(TechIndex[m])
 
 
-getTechIndex2018('State Technology and Sciencxe Index Y2018.csv')
+getTechIndex(['State Technology and Sciencxe Index Y2018.csv',
+              'State Technology and Sciencxe Index Y2016.csv'])
 
 
-def download2017Census(vars):
+def downloadCensus(vars):
     # Dowload Year 2017 data from American Community Census website throught the official API
     # Since the H1b filings disclosed in Year2018 contains
     # Call this function while downloading Censusdata
     statedata = []
-    for i in range(len([2017])):
+    for i in range(len([2017, 2015])):
         statedata.append(censusdata.download(
-            'acs1', [2017][i], censusdata.censusgeo([('state', '*')]), vars))
+            'acs1', [2017, 2015][i], censusdata.censusgeo([('state', '*')]), vars))
     return statedata
 
 
-statedata = download2017Census(
+statedata = downloadCensus(
     ['B07009_036E', 'B07009_035E', 'B07009_034E', 'B07009_033E', 'B07009_032E'])
 # Variables' meaning refers to "https://api.census.gov/data/2015/acs/acs1/subject/variables"
 # B07009_036E Moved from abroad!!Graduate or professional degree
@@ -130,16 +137,17 @@ def derivedColumns(statedata):
         statedata[i]['percent_move_from_abroad_above_college_degree'] = (
             statedata[i].iloc[:, 0]+statedata[i].iloc[:, 1])/statedata[i]['total_move_from_abroad']*100
         # Drop features that we no long need
-        statedata = statedata[i].drop(['move_from_abroad_Graduate_professional_degree_population_size',
-                                       'move_from_abroad_Bachelor_degree_population_size',
-                                       'move_from_abroad_college_associate_degree_population_size',
-                                       'move_from_abroad_high_school_degree_population_size',
-                                       'move_from_abroad_less_than_high_school_degree_population_size',
-                                       'total_move_from_abroad'], axis=1)
+        statedata[i] = statedata[i].drop(['move_from_abroad_Graduate_professional_degree_population_size',
+                                          'move_from_abroad_Bachelor_degree_population_size',
+                                          'move_from_abroad_college_associate_degree_population_size',
+                                          'move_from_abroad_high_school_degree_population_size',
+                                          'move_from_abroad_less_than_high_school_degree_population_size',
+                                          'total_move_from_abroad'], axis=1)
         # Since we only extract one year data, meaning the length of statedata is 1.
         # Therefore we could store statedata[i] by statedata directly.
         return statedata
 
 
 statedata = derivedColumns(statedata)
-statedata.to_csv('clean_Census_State_data.csv', index=False)
+statedata[0].to_csv('2017_Census_State_data.csv', index=False)
+statedata[1].to_csv('2015_Census_State_data.csv', index=False)
